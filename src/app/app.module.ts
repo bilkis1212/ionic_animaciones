118890cae5f7e1024d18f8030c26479e936efd74
bilkis1212 <nicco.moreira03@gmail.com>
2021-09-15 13:12:15 -0300

    confirmed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },BarcodeScanner ,Base64ToGallery],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/ani3/ani3.page.ts
+++ src/app/pages/ani3/ani3.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AnimationController, Animation } from '@ionic/angular';
 @Component({
   selector: 'app-ani3',
   templateUrl: './ani3.page.html',
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Ani3Page implements OnInit {
 
-  constructor() { }
+  animation: Animation;
+
+  constructor(private animationCtrl: AnimationController) { }
 
   ngOnInit() {
+    this.animation = this.animationCtrl.create()
+      .addElement(document.querySelector('.square'))
+      .duration(1500)
+      .iterations(Infinity)
+      .fromTo('transform', 'translateX(0px)', 'translateX(100px)')
+      .fromTo('opacity', '1', '0.2');
+  }
+  play(){
+    this.animation.play();
+
+  }
+  pause(){
+    this.animation.pause();
+
+  }
+  stop(){
+    this.animation.stop();
+    
   }
 
 }
